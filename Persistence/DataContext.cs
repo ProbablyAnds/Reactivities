05de84dee08f0421a399e2539743cf791d052e0e
ProbablyAnds <andrew.codner.work@gmail.com>
2023-02-10 14:33:49 +0200

    End of section 12

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext    //represents a session wiht the database + save instances with the entities
+    public class DataContext : IdentityDbContext<AppUser>    //represents a session wiht the database + save instances with the entities
     {
         public DataContext(DbContextOptions options) : base(options)
         {
